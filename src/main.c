ba3591eebbf6c0825c05b919d07a712f6b392f31
Clement Senand <csenand@c1r8p11.42quebec.com>
2023-05-11 16:06:02 -0400

    Main | main modified

--- src/main.c
+++ src/main.c
@@ -3,7 +3,11 @@
 #define GWEN 0
 #define LOULOU 0
 
-
+/*
+** Main
+** prints a welcome message on the terminal
+** Infinite loop pour attendre le user input
+*/
 int main(void)
 {
 	t_minishell	*parse;
@@ -20,19 +24,3 @@ int main(void)
 	}
 	ft_clean_up(parse);
 }
-
-/*
-** Simple main that prints a message (printf) on the terminal
-** Infinite loop pour attendre le user input
-*/
-/* int main()
-{
-	t_minishell	*pars;
-
-	printf("\n😈😈😈 Welcome to minishell ... should I say " RED"MINIHELLLL 😈😈😈\n\n"WHT);
-	pars = ft_init_pars();
-	while (1)
-		pars->line = readline("Minishell > ");
-	ft_clean_up(pars);
-}
- */
