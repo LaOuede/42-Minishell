dcec297393861b202bfa97a70300648725976f4f
Clement Senand <csenand@c1r8p11.42quebec.com>
2023-05-15 15:17:47 -0400

    Exec | exec_pipes.c added

---
+++ src/exec_pipes.c
@@ -0,0 +1,37 @@
+#include "../include/minishell.h"
+#include "../include/execution.h"
+
+void	ft_close_pipes(t_exec *exec)
+{
+	int	i;
+
+	i = -1;
+	while (++i < exec->pipes_nb)
+	{
+		close(exec->pipes[i][0]);
+		close(exec->pipes[i][1]);
+	}
+}
+
+int	ft_create_pipes(t_exec *exec)
+{
+	int	i;
+
+	if (exec->cmd_nb < 1)
+		return (0);
+	exec->pipes = ft_calloc(exec->pipes_nb, sizeof(int *));
+	if (!exec->pipes)
+		ft_err("Something went wrong during calloc", exec);
+	i = -1;
+	while (++i < exec->pipes_nb)
+	{	
+		exec->pipes[i] = ft_calloc(2, sizeof(int));
+		if (!exec->pipes[i])
+			ft_err("Something went wrong during pipe creation: ", exec);
+	}
+	i = -1;
+	while (++i < exec->pipes_nb)
+		if (pipe(exec->pipes[i]) == -1)
+			ft_err("Something went wrong during pipe creation: ", exec);
+	return (1);
+}
