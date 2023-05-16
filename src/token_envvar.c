254c922147bedaca8e0780ff482a0d022d3c1be2
Gwen <gwenola.leroux@gmail.com>
2023-05-16 11:16:13 -0400

    token_quotes.c | $ handling added

--- src/token_envvar.c
+++ src/token_envvar.c
@@ -11,7 +11,6 @@ void	ft_envvar_token(int *i, t_minishell *parse)
 	while (parse->input[++(*i)])
 	{
 		tmp = ft_stock_char(tmp, parse->input[(*i)]);
-		printf("char = %c\n", parse->input[(*i)]);
 		if (ft_ismetac(parse->input[(*i)]) == 1)
 			break ;
 	}
@@ -21,11 +20,42 @@ void	ft_envvar_token(int *i, t_minishell *parse)
 	while (parse->envp[j] && ft_strncmp(parse->envp[j], tmp, len) != 0)
 		j++;
 	if (!parse->envp[j])
+	{
+		write(1, "\n", 1);
 		return ;
+	}
 	tmp = NULL;
 	k = len - 1;
 	while (parse->envp[j][k++])
 		tmp = ft_stock_char(tmp, parse->envp[j][k]);
 	ft_add_token_bottom(&parse->line, ft_create_node(tmp, parse));
 	free(tmp);
+}
+
+char	*ft_envvar_quotes_token(int *i, t_minishell *parse, char *str)
+{
+	char	*tmp;
+	int		j;
+	int		k;
+	size_t	len;
+
+	tmp = NULL;
+	while (parse->input[++(*i)] && ft_ismetac(parse->input[(*i)]) == 0)
+		tmp = ft_stock_char(tmp, parse->input[(*i)]);
+	tmp = ft_stock_char(tmp, '=');
+	j = 0;
+	len = ft_strlen(tmp);
+	while (parse->envp[j] && ft_strncmp(parse->envp[j], tmp, len) != 0)
+		j++;
+	if (!parse->envp[j])
+	{
+		if (!str)
+			write(1, "\n", 1);
+		return (NULL);
+	}
+	tmp = NULL;
+	k = len - 1;
+	while (parse->envp[j][k++])
+		tmp = ft_stock_char(tmp, parse->envp[j][k]);
+	return (tmp);
 }
