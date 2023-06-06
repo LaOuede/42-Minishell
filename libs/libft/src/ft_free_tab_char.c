61870a27d3b938ad3581fc30e02a939c153955c8
Clement Senand <csenand@c1r8p11.42quebec.com>
2023-06-06 09:41:03 -0400

    Libft | ft_free_tab_char modifed to add ft_freenull instead of C function free

--- libs/libft/src/ft_free_tab_char.c
+++ libs/libft/src/ft_free_tab_char.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ft_free_tab_char.c                                 :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: gle-roux <gle-roux@student.42.fr>          +#+  +:+       +#+        */
+/*   By: csenand <csenand@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/10 13:51:24 by gwenolalero       #+#    #+#             */
-/*   Updated: 2023/04/06 09:00:45 by gle-roux         ###   ########.fr       */
+/*   Updated: 2023/06/06 09:39:38 by csenand          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,7 +18,7 @@ char	*ft_free_tab_char(char **tab)
 
 	i = 0;
 	while (tab[i])
-		free(tab[i++]);
-	free(tab);
+		ft_freenull(tab[i++]);
+	ft_freenull(tab);
 	return (0);
 }
