2f65179c4c686f0782931181ff2d5eb861571567
Clmntsnd <clement.senand@gmail.com>
2023-06-07 11:04:31 -0400

    Folder exec created and change Makefile accordingly 2

---
+++ src/exec/exec.c
@@ -0,0 +1,179 @@
+#include "../../include/minishell.h"
+
+void	ft_is_redirection(t_exec *exec, t_jct *jct)
+{
+	char 	***cmds_tab;
+	int		i;
+	//int		j;
+
+	if(!jct->tab)
+		return ;
+	cmds_tab = jct->tab;
+	i = 0;
+	printf("---		ft_is_redirection starts	---\n");
+	i = 0;
+	while (cmds_tab[i])
+	{
+		//j = 0;
+		if(cmds_tab[i][2])
+		{
+			exec->input_file_name = ft_strtrim(ft_strrchr(cmds_tab[i][2],'<'), "< ");
+			exec->fl_redirin = 1;
+			printf("exec->input_file_name : %s\n", exec->input_file_name);
+			exec->input = open(exec->input_file_name, O_RDONLY);
+		}
+		if(cmds_tab[i][3])
+		{
+			exec->output_file_name = ft_strtrim(ft_strrchr(cmds_tab[i][3],'>'), "> ");
+			exec->fl_redirout = 1;
+			printf("exec->output_file_name : %s\n", exec->output_file_name);
+			exec->output = open(exec->output_file_name, O_RDWR | O_CREAT | O_TRUNC, 0644);
+		}
+		i++;
+	}
+	// exec->fl_hdr = 1; // >>
+	// exec->fl_hdl = 0; // <<
+	// 	//TODO to modify the below
+	// if (exec->fl_redirout == 1 && exec->fl_hdr == 1)
+	// 	exec->output = open("out", O_RDWR | O_CREAT | O_APPEND, 0644);
+	// else
+	// 	exec->output = open("out", O_RDWR | O_CREAT | O_TRUNC, 0644);
+	// 	//TODO to modify the above
+	// exec->output = open("out", O_RDWR | O_CREAT | O_TRUNC, 0644);
+	// //TODO remove the above
+	printf("---		ft_is_redirection ends	---\n");
+}
+
+void	ft_make_pids(t_exec *exec, t_jct *jct)
+{
+	int	i;
+	
+	exec->pids = ft_calloc(exec->cmd_nb, sizeof(pid_t *));
+	if (!exec->path_var)
+		return ;
+	i = -1;
+	while (++i < exec->cmd_nb)
+	{
+		exec->pids[i] = fork();
+		if (exec->pids[i] == -1)
+			perror("Something went wrong during pid process:");
+		// printf("--- Enter in ft_chils_proc	---\n");
+		if (exec->pids[i] == 0)
+		{
+			//this is the child process
+			ft_dup_process(exec, i);
+			ft_run_cmd(exec, jct, i);
+		}	
+		//TODO put waitpid here, maybe ?
+		// printf("--- Exit ft_chils_proc	---\n");
+	}
+	//TODO close all input and/or output here (including here_doc)
+	if(exec->fl_redirin == 1)
+		close(exec->input);
+	if(exec->fl_redirout == 1)
+		close(exec->output);
+	ft_close_pipes(exec);
+}
+
+char	*ft_cmd_path(t_exec *exec, char *cmds)
+{
+	char	*path;
+	int		i;
+
+	if (access(cmds, F_OK | X_OK) == 0)
+		return (ft_strdup(cmds));
+	path = ft_strjoin("./", cmds);
+	if (access(path, F_OK | X_OK) == 0)
+		return (path);
+	if (path)
+		free(path);
+	i = -1;
+	while (exec->path_var[++i])
+	{
+		path = ft_strjoin(exec->path_var[i], cmds);
+		if (access(path, F_OK | X_OK) == 0)
+			return (path);
+		if (path)
+			free(path);
+	}
+	ft_err("Error ! Can't find path to program", exec);
+	path = NULL;
+	return (path);
+}
+
+void	ft_run_cmd(t_exec *exec, t_jct *jct, int r)
+{
+	char	*path;
+	char	***cmds;
+
+	cmds = jct->tab;
+	// fprintf(stderr, "cmd[%d][0] = %s\n", r, cmds[r][0]);
+	// fprintf(stderr, "cmd[%d][1] = %s\n", r, cmds[r][1]);
+	// fprintf(stderr, "cmd[%d][2] = %s\n", r, cmds[r][2]);
+	// fprintf(stderr, "cmd[%d][3] = %s\n", r, cmds[r][3]);
+	path = ft_cmd_path(exec, cmds[r][0]);
+	fprintf(stderr, "path = %s\n", path);
+	if (!path)
+		free(path);
+	else if (execve(path, cmds[r], exec->envp) < 0)
+		perror("Error ! Something went wrong while executing: ");
+	//TODO ft_err exit if there is an error, so the below will never be executed	
+}
+
+void	ft_dup_process(t_exec *exec, int i)
+{
+	exec->index = i;
+	if (exec->index == 0)
+	{
+		if (exec->fl_redirin == 1)
+			dup2(exec->input, STDIN_FILENO);
+		else
+			dup2(0, STDIN_FILENO);
+	}
+	else
+		dup2(exec->pipes[i - 1][0], STDIN_FILENO);
+	if (exec->index == exec->cmd_nb - 1)
+	{
+		if(exec->fl_redirout == 1)
+			dup2(exec->output, STDOUT_FILENO);
+		dup2(1, STDOUT_FILENO);
+	}
+	else
+		dup2(exec->pipes[i][1], STDOUT_FILENO);
+	//TODO close all input and/or output here (including here_doc)
+	if(exec->fl_redirin == 1)
+		close(exec->input);
+	if(exec->fl_redirout == 1)
+		close(exec->output);
+	ft_close_pipes(exec);
+}
+
+void	ft_exec(t_exec *exec, t_jct *jct)
+{
+	int	i;
+	int status;
+	
+	ft_is_redirection(exec, jct);
+	if (ft_create_pipes(exec) == 2)
+		return ;
+	ft_make_pids(exec, jct);
+	i = -1;
+	while (++i < exec->cmd_nb)
+		waitpid(exec->pids[i], &status, 0);
+	//TODO clarifier le 2nd arg de waitpid
+}
+
+// void	ft_exec_jct(t_exec *exec, t_jct *jct)
+// {
+// 	int	i;
+// 	int status;
+	
+// 	ft_is_redirection(exec, jct);
+// 	ft_create_pipes(exec);
+// 	ft_make_pids_jct(exec, jct);
+// 	i = -1;
+// 	while (++i < exec->cmd_nb)
+// 		waitpid(exec->pids[i], &status, 0);
+// 	//TODO clarifier le 2nd arg de waitpid
+// 	// ft_free_data(exec);
+// }
---
+++ src/exec/exec_utils.c
@@ -0,0 +1,220 @@
+#include "../../include/minishell.h"
+
+# define PRINT_DEBUG 0
+
+void	ft_print_debug(t_exec *exec)
+{
+	if (PRINT_DEBUG)
+	{
+		int j = -1;
+
+		//Printing What's inside 'exec->readline' variable
+		printf(BLD);
+		printf("\n---------------------------------------------------\n");
+		printf("---		Print_debug "GRN"starts"RESET BLD"		---\n");
+		printf("---------------------------------------------------"RESET"\n\n");
+
+		printf("---------------------------------------------------\n");
+		printf("---	Printing exec->readline[i]		---\n");
+		printf("|\n");
+		while(exec->readline[++j])
+			printf("|	exec->readline[%d] : %s\n", j, exec->readline[j]);
+		printf("|\n");
+		printf("---	Printing exec->readline ends		---\n");
+		printf("---------------------------------------------------\n\n");
+
+		printf("---------------------------------------------------\n");
+		printf("---	Printing exec->cmd_nb			---\n");
+		printf("|\n");
+		printf("|	cmd_nb = %d\n", exec->cmd_nb);
+		printf("|	pipes_nb = %d\n", exec->pipes_nb);
+		printf("|\n");
+		printf("---	Printing exec->cmb_nb ends		---\n");
+		printf("---------------------------------------------------\n\n");
+
+		printf("---------------------------------------------------\n");
+		printf("---	Printing exec->pipe_op & flag		---\n");
+		printf("|\n");
+		printf("|	pipe_op = %d\n", exec->pipes_op);
+		printf("|	fl_pipe_op = %d\n", exec->fl_pipe_op);
+		printf("|\n");
+		printf("---	Printing exec->pipe_op & flag ends	---\n");
+		printf("---------------------------------------------------\n\n");
+
+		printf("---------------------------------------------------\n");
+		printf("---	Printing input_file_name & flag		---\n");
+		printf("|\n");
+		printf("|	input_file_name = %s\n", exec->input_file_name);
+		printf("|	fl_redirin = %d\n", exec->fl_redirin);
+		printf("|\n");
+		printf("---	Printing input_file_name & flag ends	---\n");
+		printf("---------------------------------------------------\n\n");
+
+		printf("---------------------------------------------------\n");
+		printf("---	Printing output_file_name & flag	---\n");
+		printf("|\n");
+		printf("|	output_file_name = %s\n", exec->output_file_name);
+		printf("|	fl_redirout = %d\n", exec->fl_redirout);
+		printf("|\n");
+		printf("---	Printing output_file_name & flag ends	---\n");
+		printf("---------------------------------------------------\n\n");
+
+		printf(BLD);
+		printf("---------------------------------------------------\n");
+		printf("---		Print_debug "RED"ends"RESET BLD"		---\n");
+		printf("---------------------------------------------------"RESET"\n\n");
+	}
+}
+
+void	ft_copy_env(t_exec *exec, char **envp)
+{
+	int	i;
+	int	j;
+
+	i = 0;
+	while (envp[i])
+		i++;
+	exec->envp = ft_calloc(sizeof(char **), i + 1);
+	i = -1;
+	j = 0;
+	while (envp[++i])
+		exec->envp[j++] = ft_strdup(envp[i]);
+	exec->envp[j] = NULL;
+}
+
+char	*ft_free_2char(char **tab)
+{
+	int	i;
+
+	i = -1;
+	while (++i < 4)
+		free(tab[i]);
+	free(tab);
+	return (0);
+}
+
+void	ft_free_3tab(t_jct *jct)
+{
+	int		i;
+	char	***tmp;
+
+	if (!jct->tab)
+		return ;
+	tmp = jct->tab;
+	i = -1;
+	while (++i < jct->cmd_nb)
+		ft_free_2char(tmp[i]);
+	free(tmp);
+	jct->tab = NULL;
+}
+
+void	ft_free_exec(t_exec *exec)
+{
+	if (exec->envp)
+		ft_free_tab_char(exec->envp);
+	if (exec->path_var)
+		ft_free_tab_char(exec->path_var);
+	if (exec->pipes)
+		ft_free_tab_int(exec->pipes, exec->pipes_nb);
+	if (exec->pids)
+		ft_freenull(exec->pids);
+	if (exec->input_file_name)
+		ft_freenull(exec->input_file_name);
+	if (exec->output_file_name)
+		ft_freenull(exec->output_file_name);
+	// if (exec->path_var)
+	// 	ft_free_tab_char(exec->path_var);
+	// ft_freenull(exec);
+}
+
+void	ft_err(char *msg, t_exec *exec)
+{
+	(void)exec;
+	// ft_free_exec(exec);
+	perror(msg);
+	// exit(1);
+}
+
+int	ft_path_var_qty(char **path_var)
+{
+	int	i;
+
+	i = 0;
+	while (path_var[i])
+		i++;
+	return (i);
+}
+
+char	**ft_get_path(char **envp, int i)
+{
+	char	**path_var;
+	char	**new_path_var;
+
+	while (envp[i] && ft_strncmp(envp[i], "PATH=", 5) != 0)
+		i++;
+	if (!envp[i])
+	{
+		new_path_var = ft_calloc(1, sizeof(char *));
+		new_path_var[0] = ft_calloc(1, sizeof(char));
+		return (new_path_var);
+	}
+	path_var = ft_split(&envp[i][5], ':');
+	if (!path_var)
+		return (NULL);
+	new_path_var = ft_calloc(ft_path_var_qty(path_var) + 1, sizeof(char *));
+	if (!new_path_var)
+		return (NULL);
+	i = -1;
+	while (path_var[++i])
+	{
+		new_path_var[i] = ft_strjoin(path_var[i], "/");
+		free(path_var[i]);
+	}
+	free(path_var);
+	return (new_path_var);
+}
+
+t_exec	*ft_init_exec(char **envp, t_jct *jct)
+{
+	static t_exec	*exec;
+
+	exec = malloc(sizeof(t_exec));
+	if (!exec)
+			perror(NULL);
+	exec->envp = NULL;
+	ft_copy_env(exec, envp);
+	exec->path_var = ft_get_path(exec->envp, 0);
+	exec->input = 0;
+	exec->input_file_name = NULL;
+	exec->output = 0;
+	exec->output_file_name = NULL;
+	exec->index = 0;
+	exec->fl_redirin = 0;
+	exec->fl_redirout = 0;
+	exec->pids = 0;
+	exec->pipes = 0;
+	exec->cmd_nb = jct->cmd_nb;
+	exec->pipes_nb = exec->cmd_nb - 1;
+	return(exec);
+}
+
+/*
+*-----------------------------------------------------------------------------------------------------------------------------------------------*
+|			|	cmds			|	args			|	redirin		|	redirout	|			|			|			|			|			|
+*-----------------------------------------------------------------------------------------------------------------------------------------------*
+|	*cmd0	|	echo cmd0[0]	|	allo cmd0[1]	|				|				|			|			|			|			|			|
+*-----------------------------------------------------------------------------------------------------------------------------------------------*
+|	*cmd1	|	cat	cmd1[0]		|					|	< Makefile	|	> outfile	|			|			|			|			|			|
+*-----------------------------------------------------------------------------------------------------------------------------------------------*
+
+each cmds[i] should be stored in a 2D array
+-> make a fct that recognize if there an operator (| , <, >, <<, >>) and switch flags 
+
+typedef struct s_jct
+{
+	char **tab;
+	int	pipe_nb;
+	int	cmd_nb;
+} 	t_jct
+
+*/
