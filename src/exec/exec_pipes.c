6f039d5e8d3f31c37a92a5cdd59e9227c4917394
Clmntsnd <clement.senand@gmail.com>
2023-06-07 11:40:23 -0400

    Modification of ft_create_pipes (norm)

--- src/exec/exec_pipes.c
+++ src/exec/exec_pipes.c
@@ -12,18 +12,15 @@ void	ft_close_pipes(t_exec *exec)
 	}
 }
 
-int	ft_create_pipes(t_exec *exec)
+int	ft_mem_pipes(t_exec *exec)
 {
 	int	i;
-
-	if (exec->cmd_nb <= 1)
-		return (1);
 	// printf("\n--- Pipes calloc starts ---\n");
 	exec->pipes = ft_calloc(exec->pipes_nb, sizeof(int *));
 	if (!exec->pipes)
 	{
 		perror("Error during pipe memory allocation");
-		return(2);
+		return(1);
 	}
 	i = -1;
 	while (++i < exec->pipes_nb)
@@ -32,10 +29,21 @@ int	ft_create_pipes(t_exec *exec)
 		if (!exec->pipes[i])
 		{
 			perror("Error during pipe memory allocation");
-			return(2);
+			return(1);
 		}
 	}
 	// printf("\n--- Pipe calloc ends	 ---\n");
+	return (0);
+}
+
+int	ft_create_pipes(t_exec *exec)
+{
+	int	i;
+
+	if (exec->cmd_nb <= 1)
+		return (1);
+	if (ft_mem_pipes(exec) == 1)
+		return (2);
 	i = -1;
 	while (++i < exec->pipes_nb)
 	{
