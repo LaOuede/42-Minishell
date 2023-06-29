e43f0e9e07c810c9b98e74a90bd985c0e8f46793
Clmntsnd <clement.senand@gmail.com>
2023-06-28 20:40:32 -0400

    Builtin | fix exit w/ one ac, refactor the function

--- src/built_in/exit.c
+++ src/built_in/exit.c
@@ -13,25 +13,28 @@ Prototype :
 void	ft_msh_exit(t_ms *ms, char **cmd)
 {
 	printf(KYEL "-------------------- FT_MSH_EXIT" KGRN " START " RESET KYEL "--------------------\n" RESET);
-	if (cmd[0] && cmd[1])
+	int	ac;
+
+	ac = ft_get_ac(cmd);
+	if (ac > 2)
 	{
-		if (0 <= ft_atoi(cmd[1]) && ft_atoi(cmd[1]) <= 255)
+		ft_error(ERR_EXI1);
+		ft_free_all(ms);
+		exit(1);
+	} 
+	if (ac == 2)
+	{
+		if (ft_str_isdigit(cmd[1]) == false)
 		{
+			ft_error(ERR_EXI2);
+			ft_exit_free(ms, 255);
+		}	
+		if (0 <= ft_atoi(cmd[1]) && ft_atoi(cmd[1]) <= 255)
 			ms->flexit = ft_atoi(cmd[1]) % 256;
-			printf("%d\n", ms->flexit);
-		}
 		ft_free_tab_char(cmd);
 		ft_exit_free(ms, ms->flexit);
 	}
-	else if (ft_str_isdigit(cmd[1]) == false)
-	{
-		ft_error(ERR_EXI2);
-		ft_exit_free(ms, 1);
-	}
-	else if (cmd[2])
-	{
-		ft_error(ERR_EXI1);
-		ft_exit_free(ms, 255);
-	}
+	ft_free_tab_char(cmd);
+	ft_exit_free(ms, 0);
 	printf(KYEL "-------------------- FT_MSH_EXIT" KRED " END " RESET KYEL "--------------------\n" RESET);
 }
