46c23fae1329db4dd8ba3e3c09843d4543cc3906
Clmntsnd <clement.senand@gmail.com>
2023-06-30 11:49:14 -0400

    EXEC | ft_msh_export done, need to test thoroughly

--- libs/libft/src/ft_free_tab_char.c
+++ libs/libft/src/ft_free_tab_char.c
@@ -6,7 +6,7 @@
 /*   By: csenand <csenand@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/10 13:51:24 by gwenolalero       #+#    #+#             */
-/*   Updated: 2023/06/06 14:28:53 by csenand          ###   ########.fr       */
+/*   Updated: 2023/06/30 10:38:34 by csenand          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,7 +18,10 @@ char	*ft_free_tab_char(char **tab)
 
 	i = 0;
 	while (tab[i])
-		free(tab[i++]);
-	free(tab);
+	{	
+		tab[i] = ft_freenull(tab[i]);
+		i++;
+	}
+	tab = ft_freenull(tab);
 	return (0);
 }
--- src/built_in/export.c
+++ src/built_in/export.c
@@ -9,6 +9,41 @@ Prototype :
 	export [name[=value] ...]
 */
 
+char	*ft_replace_var(t_ms *ms, char *cmd, int index_var)
+{
+	char	*new_envp;
+
+	new_envp = ft_strdup(cmd);
+	free(ms->envp[index_var]);
+	return(new_envp);
+}
+
+char	**ft_export_var(t_ms *ms, char **env, char *cmd)
+{
+	char	**new_envp_env;
+	int		i;
+	int		j;
+	int		len_env;
+	int		len_env2 = 0;
+
+
+	i = 0;
+	j = 0;
+	len_env = ft_get_ac(env);
+	printf("len: %d\n", len_env);
+	new_envp_env = ft_calloc_msh(len_env + 2, sizeof(char *), ms);
+	printf("len2: %d\n", len_env2);
+	while(env[i])
+		new_envp_env[j++] = ft_strdup(env[i++]);
+	printf("cmd; %s\n", cmd);
+	printf("j: %d\n", j);
+	new_envp_env[j++] = ft_strdup(cmd);
+	new_envp_env[j] = NULL;
+	printf("j: %d\n", j);
+	ft_free_tab_char(env);
+	return (new_envp_env);
+}
+
 int		ft_find_index_var(t_ms *ms, char *cmd)
 {
 	int i;
@@ -30,32 +65,6 @@ int		ft_find_index_var(t_ms *ms, char *cmd)
 	return (0);
 }
 
-char	**ft_export_var(t_ms *ms, char **env, char *cmd)
-{
-	char	**new_env;
-	int		i;
-	int		j;
-	int		len_env;
-	int		len_env2 = 0;
-
-
-	i = 0;
-	j = 0;
-	len_env = ft_get_ac(env);
-	printf("len: %d\n", len_env);
-	new_env = ft_calloc_msh(len_env + 2, sizeof(char *), ms);
-	printf("len2: %d\n", len_env2);
-	while(env[i])
-		new_env[j++] = ft_strdup(env[i++]);
-	printf("cmd; %s\n", cmd);
-	printf("j: %d\n", j);
-	new_env[j++] = ft_strdup(cmd);
-	new_env[j] = NULL;
-	printf("j: %d\n", j);
-	ft_free_tab_char(env);
-	return (new_env);
-}
-
 bool	ft_valid_ex(char *cmd)
 {
 	if ((!ft_isalpha(cmd[0]) && cmd[0] != '_') || !ft_strchr(cmd, '='))
@@ -73,7 +82,6 @@ void	ft_msh_export(t_ms *ms, char **cmd)
 	int	index_var;
 
 	ac = ft_get_ac(cmd);
-	printf("ac; %d\n", ac);
 	i = 0;
 	if (ac == 1)
 		while ((ms->envp[++i]))
@@ -82,17 +90,11 @@ void	ft_msh_export(t_ms *ms, char **cmd)
 	while (cmd[++i])
 	{
 		if (!ft_valid_ex(cmd[i]))
-		{
-			if (ms->jct->cmd_nb == 1)
-				return ;
-			else
-				ft_exit_free(ms, 1, 0);
-		}
-		printf("ft_export_var starts\n");
+			return ;
 		index_var = ft_find_index_var(ms, cmd[i]);
-		printf("index_var: %d\n", index_var);
-		ms->envp = ft_export_var(ms, ms->envp, cmd[i]);
-		printf("ft_export_var ends\n");
-		//TODO do the rest
+		if (index_var > 0)
+			ms->envp[index_var] = ft_replace_var(ms, cmd[i], index_var);
+		else
+			ms->envp = ft_export_var(ms, ms->envp, cmd[i]);
 	}
 }
