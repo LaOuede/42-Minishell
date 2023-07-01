97d1d42ad5fa806d28a8ba571351f474daf0573d
Clmntsnd <clement.senand@gmail.com>
2023-07-01 18:35:51 -0400

    Builtin | unset updated, should work correctly, need to check the memory

--- src/built_in/exit.c
+++ src/built_in/exit.c
@@ -12,14 +12,14 @@ Prototype :
 
 void	ft_msh_exit(t_ms *ms, char **cmd)
 {
-	printf(KYEL "-------------------- FT_MSH_EXIT" KGRN " START " RESET KYEL "--------------------\n" RESET);
+	if (DEBUG)
+		printf(KYEL "-------------------- FT_MSH_EXIT" KGRN " START " RESET KYEL "--------------------\n" RESET);
 	int	ac;
 
 	ac = ft_get_ac(cmd);
 	if (ac > 2)
 	{
 		ft_error(ERR_EXI1);
-		// ft_free_all(ms);
 		return ;
 	}
 	if (ac == 2)
@@ -32,12 +32,11 @@ void	ft_msh_exit(t_ms *ms, char **cmd)
 		}
 		if (0 <= ft_atoi(cmd[1]) && ft_atoi(cmd[1]) <= 255)
 			ms->flexit = ft_atoi(cmd[1]) % 256;
-		// ft_free_tab_char(cmd);
 		ft_banner_exit(ms);
 		ft_exit_free(ms, ms->flexit, 0);
 	}
-	//ft_free_tab_char(cmd);
 	ft_banner_exit(ms);
 	ft_exit_free(ms, 0, 0);
-	printf(KYEL "-------------------- FT_MSH_EXIT" KRED " END " RESET KYEL "--------------------\n" RESET);
+	if (DEBUG)
+		printf(KYEL "-------------------- FT_MSH_EXIT" KRED " END " RESET KYEL "--------------------\n" RESET);
 }
